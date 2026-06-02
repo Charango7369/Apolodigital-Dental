7c13c9612ee39e91e6f682764a41322f3c5b0a70
Charango7369 <edwinjrivero@gmail.com>
2026-06-02 16:02:04 -0400

    Fix: ajustar UserResponse para soportar id UUID y tenant_id int

--- app/modules/users/schemas.py
+++ app/modules/users/schemas.py
@@ -1,20 +1,21 @@
 # app/modules/users/schemas.py
 from pydantic import BaseModel, EmailStr
+from uuid import UUID  # 👈 Reimportamos UUID para el ID del usuario
 
 class UserCreate(BaseModel):
     full_name: str
     email: EmailStr
     password: str
     role: str
-    tenant_id: int  # 👈 Agregado para que Swagger te pida el ID de la clínica
+    tenant_id: int  # 👈 Sigue siendo int porque la clínica usó el ID 2
 
 
 class UserResponse(BaseModel):
-    id: int  # 👈 ¡CORREGIDO! Cambiado de UUID a int para coincidir con tu base de datos
+    id: UUID  # 👈 ¡CORREGIDO! El usuario genera un UUID (ej: "de305d54...")
     full_name: str
     email: EmailStr
     role: str
-    tenant_id: int  # 👈 Añadido también aquí para saber a qué clínica pertenece al consultar
+    tenant_id: int  # 👈 Sigue siendo int para emparejar con la clínica
 
     model_config = {
         "from_attributes": True
