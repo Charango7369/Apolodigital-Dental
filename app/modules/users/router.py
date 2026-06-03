d46509ff5d394734ca4776eece95ab76c966b873
Charango7369 <edwinjrivero@gmail.com>
2026-06-03 14:23:48 -0400

    Security: Implementar endpoint /users/me para activar candado de autenticación

--- app/modules/users/router.py
+++ app/modules/users/router.py
@@ -1,5 +1,7 @@
 # app/modules/users/router.py
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
+
+#from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 import traceback  # 👈 Importamos la herramienta de rastreo
 
@@ -7,6 +9,11 @@ from app.db.session import get_db
 from app.modules.users.schemas import UserCreate
 from app.modules.users.repository import UserRepository
 from app.modules.users.service import UserService
+# 🚨 Importamos el guardián que acabamos de crear
+from app.modules.auth.dependencies import get_current_user
+from app.modules.users.models import User as UserModel
+from app.modules.users.schemas import UserResponse # Tu esquema de respuesta de usuario
+
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
@@ -23,3 +30,12 @@ async def create_user(payload: UserCreate, db: AsyncSession = Depends(get_db)):
         traceback.print_exc()  # Esto imprimirá el error real completo en tu consola de Railway
         print("❌ ======================================================= ❌\n")
         raise e  # Mantiene el error 500 para el cliente pero ya lo dejó grabado
+    
+
+# 🔐 ESTE ENDPOINT ACTIVARÁ EL CANDADO EN SWAGGER
+@router.get("/me", response_model=UserResponse)
+async def get_my_profile(current_user: UserModel = Depends(get_current_user)):
+    """
+    Devuelve el perfil del usuario autenticado basado en el Token JWT.
+    """
+    return current_user
